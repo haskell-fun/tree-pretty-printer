19070c2fc0cf22073f06e163ea99a50bcf99d6e3
albertprz <albertoperez1994@gmail.com>
2022-11-09 13:50:26 +0100

    feat: simple Tree data struct and unit tests

--- src/App.hs
+++ src/App.hs
@@ -1,4 +1,22 @@
 module App where
 
+import Data.List.NonEmpty
+
+
 program :: IO ()
-program = putStrLn "hello!"
+program =
+  pure ()
+
+
+data Tree a = Leaf a | Branch a (NonEmpty (Tree a))
+  deriving (Show, Eq)
+
+
+pretty :: Show a => Tree a -> String
+pretty _ = ""
+
+
+
+-- enum Tree [T]:
+--   case Leaf (value: T)
+--   case Branch (value: T) (children: NonEmptyList[Tree[T]])
--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -5,12 +5,24 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck.Property
 
+import Data.List.NonEmpty
+
+
+tree1 = Leaf "a"
+
+tree2 = Branch "a" (Leaf "b" :| [Leaf "c"])
+
+
+
+
 spec :: Spec
 spec = describe "Simple test" $ do
 
-     it "example-based unit test" $
-        1 `shouldBe` 1
+     it "simple depth 1 tree unit test" $
+        pretty tree1 `shouldBe` "a"
 
-     prop "property-based unit test" $
-        \l -> reverse ( reverse l ) == ( l::[Int])
+     it "depth 2 tree unit test" $
+        pretty tree2 `shouldBe` "a\n--b\n--c"
 
+     -- prop "property-based unit test" $
+     --    \l -> reverse ( reverse l ) == ( l::[Int])
